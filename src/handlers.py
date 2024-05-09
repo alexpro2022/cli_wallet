d4900c289c26c52d34cd2977964f10f051a71675
Alex Proskuriakov <alexpro1972@yandex.ru>
2024-05-09 18:43:40 +0300

    Update Readme

--- src/handlers.py
+++ src/handlers.py
@@ -45,42 +45,6 @@ def balance(file_path: str) -> tuple[str, str, str]:
 
 
 def edit_record(file_path: str) -> None:
-    """def choose_row(df, pagination: int = c.PAGINATION):
-        start_pos = 0
-        max_pos = len(df) - pagination
-        prompt = c.EDIT_MSG
-        while True:
-            print(df[start_pos : start_pos + pagination])
-            inp, _ = parse(input(prompt))
-            prompt = c.EDIT_MSG
-            match inp:
-                case c.UP_CMD:
-                    start_pos -= pagination
-                    if start_pos < 0:
-                        start_pos = 0
-                        prompt = c.NO_SCROLL_UP_MSG
-                case c.DOWN_CMD:
-                    start_pos += pagination
-                    if start_pos > max_pos:
-                        start_pos = max_pos
-                        prompt = c.NO_SCROLL_DOWN_MSG
-                case _:
-                    try:
-                        return int(inp)
-                    except ValueError:
-                        prompt = c.SOMETHING_WRONG_MSG
-
-    def get_new_data() -> None:
-        # TODO: values validation (sum - to convert to Decimal)
-        data = []
-        while True:
-            inp = input(c.INPUT_NEW_DATA_MSG)
-            if inp == "/q":
-                return data
-            col_name, value = inp.split(" ")
-            if col_name in c.CSV_HEADER:
-                data.append((col_name, value))"""
-
     df = pd.read_csv(file_path)
     row = choose_row(df)
     new_data = get_new_data()
